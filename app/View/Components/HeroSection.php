8e2955bbf269e9749baae1d39ab03fcd3288f814
eisenblume7 <zainzidan100@gmail.com>
2024-11-12 22:48:16 +0700

    Menambahkan Section Hero

---
+++ app/View/Components/HeroSection.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class HeroSection extends Component
+{
+    public function render()
+    {
+        return view('components.hero-section');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,10 @@
     <!-- Tambahkan Bootstrap Icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
 
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+
+
     <!-- CSS Khusus Navbar -->
     <style>
         .navbar-brand {
@@ -39,20 +43,35 @@
             font-weight: 500;
         }
 
+        .btn-outline-green {
+            border-radius: 0;
+            padding: 5px 15px;
+            font-weight: 500;
+            background-color: #2f855a;
+            color: white;
+        }
+
         .btn-outline-success:hover {
             color: white;
             background-color: #198754;
         }
+
+        .btn-outline-green:hover {
+            border-radius: 0;
+            padding: 5px 15px;
+            font-weight: 500;
+            background-color: #245e42;
+            color: white;
+        }
+
     </style>
 </head>
 <body>
 <!-- Panggil komponen Navbar -->
 <x-navbar />
 
-<div class="container mt-5">
-    <h1>Welcome to EcoQ</h1>
-    <p>This is the content of the welcome page.</p>
-</div>
+<x-hero-section />
+
 
 <!-- Tambahkan Bootstrap Bundle JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
